b42c379972d297d766b6a4459bd7d3c4d8d6d1f6
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-09-18 21:48:40 -0500

    server/api: adding api prefix

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express'
 
 const router = express.Router();
 
-router.get('/', (_req: Request, res: Response) => {
+router.get('/api/todo', (_req: Request, res: Response) => {
   res.send({ hello: 'world' })
 });
 
