552bd1375f235aa0500162c1626a9dc2d35fe7c8
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-09-21 21:41:52 -0500

    server/test: update testing

--- server/src/routes/__test__/index.test.ts
+++ server/src/routes/__test__/index.test.ts
@@ -2,7 +2,5 @@ import request from "supertest";
 import { app } from "../../app";
 
 describe('index route', () => {
-  it('Should return the TODO list', () => {
-    return request(app).get('/api/todos').expect(200)
-  })
+  it.todo('Should return the TODO list')
 })
